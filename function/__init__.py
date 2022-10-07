bfe1a0e6413c01d97ad625ad6313a86c5bd8c1ad
Pamela Fox <pamela.fox@gmail.com>
2022-10-07 14:54:26 -0700

    Get it deployed correctly

--- function/__init__.py
+++ function/__init__.py
@@ -1,5 +1,6 @@
 import json
 
+import sklearn
 import azure.functions as func
 import joblib
 import numpy as np
@@ -17,7 +18,7 @@ def get_param(req, param_name):
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    loaded_model = joblib.load('yearly-comp.pkl')
+    loaded_model = joblib.load('function/yearly-comp.pkl')
     X_new = np.array([[8, 6, 'Bachelor’s degree (B.A., B.S., B.Eng., etc.)','I am a developer by profession', 'Employed, full-time', 'United States of America']])
     result = loaded_model.predict(X_new)
     return func.HttpResponse(json.dumps({"salary": result[0]}))
